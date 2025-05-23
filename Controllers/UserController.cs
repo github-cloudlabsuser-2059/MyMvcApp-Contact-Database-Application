989182e9bfa545940aaa3166c47fbd2d620c550e
odl_user_1732159@azurehol4699.onmicrosoft.com <odl_user_1732159@azurehol4699.onmicrosoft.com>
2025-05-23 16:30:26 +0000

    CRUD

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,59 +8,87 @@ public class UserController : Controller
 {
     public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
 
-        // GET: User
-        public ActionResult Index()
+    // GET: User
+    public ActionResult Index()
+    {
+        return View(userlist);
+    }
+
+    // GET: User/Details/5
+    public ActionResult Details(int id)
+    {
+        return View(userlist[id]);
+        // Implement the details method here
+    }
+
+    // GET: User/Create
+    public ActionResult Create()
+    {
+        return View();
+        //Implement the Create method here
+    }
+
+    // POST: User/Create
+    [HttpPost]
+    public ActionResult Create(User user)
+    {
+        // This method is responsible for handling the HTTP POST request to create a new user.
+        // It receives user input from the form submission and adds it to the userlist.
+        // If the model state is valid, it redirects to the Index action to display the updated list of users.
+        // If there are validation errors, it returns the Create view to display those errors.
+
+        if (ModelState.IsValid)
         {
-            // Implement the Index method here
+            userlist.Add(user);
+            return RedirectToAction("Index");
         }
 
-        // GET: User/Details/5
-        public ActionResult Details(int id)
-        {
-            // Implement the details method here
-        }
+        return View(user);
+    }
 
-        // GET: User/Create
-        public ActionResult Create()
-        {
-            //Implement the Create method here
-        }
+    // GET: User/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View(userlist[id]);
+        // This method is responsible for displaying the view to edit an existing user with the specified ID.
+        // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+    }
 
-        // POST: User/Create
-        [HttpPost]
-        public ActionResult Create(User user)
+    // POST: User/Edit/5
+    [HttpPost]
+    public ActionResult Edit(int id, User user)
+    {
+        if (ModelState.IsValid)
         {
-            // Implement the Create method (POST) here
+            var existingUser = userlist.ElementAtOrDefault(id);
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Add other properties as needed
+                return RedirectToAction("Index");
+            }
+            return NotFound();
         }
+        return View(user);
+    }
 
-        // GET: User/Edit/5
-        public ActionResult Edit(int id)
-        {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
-        }
+    // GET: User/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View(userlist[id]);
+        // Implement the Delete method here
+    }
 
-        // POST: User/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, User user)
+    // POST: User/Delete/5
+    [HttpPost]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        if (id >= 0 && id < userlist.Count)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            userlist.RemoveAt(id);
+            return RedirectToAction("Index");
         }
-
-        // GET: User/Delete/5
-        public ActionResult Delete(int id)
-        {
-            // Implement the Delete method here
-        }
-
-        // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            // Implement the Delete method (POST) here
-        }
+        return NotFound();
+    }
 }
